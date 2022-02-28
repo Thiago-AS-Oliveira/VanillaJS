ddd77735f4aa54e5a16232c621582ecb8540afb6
Thiago-AS-Oliveira <thiago_a-f@hotmail.com>
2022-02-28 19:57:26 -0300

    pagina de reviews

---
+++ paginaDeReviews/app.js
@@ -0,0 +1,81 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "Susan Smith",
+      job: "Web Developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "Anna Johnson",
+      job: "Web Designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "Peter Jones",
+      job: "Intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "Bill Anderson",
+      job: "The Boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ]
+
+
+function addReviews(reviews) {
+  let img = document.querySelector(".usuarioImg")
+  let autor = document.querySelector(".autor")
+  let cargo = document.querySelector(".cargo")
+  let texto = document.querySelector(".texto")
+
+  img.setAttribute("src", reviews.img)
+  autor.innerText = reviews.name
+  cargo.innerText = reviews.job
+  texto.innerText = reviews.text
+}
+
+let indice = Math.floor(Math.random() * reviews.length)
+addReviews(reviews[indice])
+
+const btnAnterior = document.querySelector(".anterior")
+btnAnterior.addEventListener("click", () => {
+  indice--
+  addReviews(reviews[indice])
+  console.log(indice)
+  if (indice == 0) {
+    indice = reviews.length
+  }
+})
+
+const btnProximo = document.querySelector(".proximo")
+btnProximo.addEventListener("click", () => {
+  indice++
+  addReviews(reviews[indice])
+  console.log(indice)
+  if (indice == 3) {
+    indice = -1
+  }
+})
+
+const btnAleatorio = document.querySelector(".review-aleatoria")
+btnAleatorio.addEventListener("click", () => {
+    indice = Math.floor(Math.random() * reviews.length)
+    addReviews(reviews[indice])
+    })
